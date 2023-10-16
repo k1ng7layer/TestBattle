ddf5ba16b32cfbb651d8958586eee2e63bdf211c
Paata Baliashvili <pl.baliashvili@gmail.com>
2023-10-16 12:59:57 +0300

    remove comments

--- Assets/Scripts/Game/Services/SceneLoading/Impl/SceneLoadingManager.cs
+++ Assets/Scripts/Game/Services/SceneLoading/Impl/SceneLoadingManager.cs
@@ -10,22 +10,10 @@ namespace Game.Services.SceneLoading.Impl
     {
         public void RestartGame()
         {
-            // var unloadingOperation = SceneManager.UnloadSceneAsync(ELevelName.Game.ToString());
-            // Observable.FromCoroutine(() => UnLoadScene(unloadingOperation))
-            //     .Subscribe(_ =>
-            //     {
-            //         // var loadingOperation =
-            //         //     SceneManager.LoadSceneAsync(ELevelName.Game.ToString(), LoadSceneMode.Single);
-            //         //
-            //         // var disposable = Observable.FromCoroutine(() => LoadScene(loadingOperation));
-            //         //.Subscribe(_ => RunContext("GameContext"));
-            //     });
-            
             var loadingOperation =
                 SceneManager.LoadSceneAsync(ELevelName.Game.ToString(), LoadSceneMode.Single);
 
-            var disposable = Observable.FromCoroutine(() => LoadScene(loadingOperation));
-
+            Observable.FromCoroutine(() => LoadScene(loadingOperation));
         }
 
         private IEnumerator LoadScene(AsyncOperation loadingOperation)
