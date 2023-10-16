98c7bcf4b0954e40994532d03f5f98b46d1e0386
Paata Baliashvili <pl.baliashvili@gmail.com>
2023-10-16 05:38:21 +0300

    add unit material fade

--- Assets/Scripts/Game/Views/Unit/Impl/UnitView.cs
+++ Assets/Scripts/Game/Views/Unit/Impl/UnitView.cs
@@ -1,13 +1,48 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 namespace Game.Views.Unit.Impl
 {
     public class UnitView : MonoBehaviour, 
         IUnitView
     {
+        [SerializeField] private Renderer meshRenderer;
+        [SerializeField] private Color targetColor;
+        [SerializeField] private float fadeDuration;
+
+        private Color _initialColor;
+
+        private void Start()
+        {
+            _initialColor = meshRenderer.material.color;
+        }
+
         public void OnTakeDamage()
         {
+            StopAllCoroutines();
             
+            StartCoroutine(ColorBlink(_initialColor, targetColor));
+        }
+
+        private IEnumerator ColorBlink(Color from, Color to)
+        {
+            float elapsedTime = 0f;
+
+            while (elapsedTime < fadeDuration / 2f)
+            {
+                elapsedTime += Time.deltaTime;
+                meshRenderer.material.color = Color.Lerp(from, to, elapsedTime / (fadeDuration /2 ));
+                yield return null;
+            }
+            
+            elapsedTime = 0f;
+            
+            while (elapsedTime < fadeDuration / 2f)
+            {
+                elapsedTime += Time.deltaTime;
+                meshRenderer.material.color = Color.Lerp(to, from, elapsedTime / (fadeDuration /2 ));
+                yield return null;
+            }
         }
     }
 }
